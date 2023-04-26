300c19e0d55d5185433c64656331fc8bd2039ae1
Adrian Dorado <rbbitphoto@gmail.com>
2023-04-26 03:25:32 -0700

    moved chakra provider to index.js, added theme to be customized

--- melody-mystery/src/index.js
+++ melody-mystery/src/index.js
@@ -1,7 +1,8 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import theme from './theme'
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
@@ -9,10 +10,10 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <StrictMode>
-    <ColorModeScript />
+  <ChakraProvider theme={theme} >
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
-  </StrictMode>
+  </ChakraProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ melody-mystery/src/theme.js
@@ -0,0 +1,36 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const config = {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+};
+
+const colors = {
+    c: {
+        blueSapphire: '#006e90',
+        carrOrange: '#f18f01',
+        turqGreen: '#a8d4ad',
+        lemonYellow: '#f2f79e',
+        maxYellow: '#e8ec67',
+    },
+};
+
+const breakpoints = {
+    'sm': '320px',
+    'md': '400px',
+    'lg': '520px',
+    'xl': '800px',
+    'xxl': '1280px',
+    'xxxl': '1920px',
+};
+
+const fonts = {
+    f: {
+        heading: 'Amatic SC',
+        paragraph: 'Amatic SC',
+    }
+};
+
+const theme = extendTheme({ config, colors, fonts, breakpoints });
+
+export default theme;
