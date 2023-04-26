46c749d04a36c3fe42ecfbe6b5ab9109b0b82548
Adrian Dorado <rbbitphoto@gmail.com>
2023-04-26 03:16:54 -0700

    windows compatability

---
+++ melody-mystery/src/components/GameCard.js
@@ -0,0 +1,12 @@
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+export default function GameCard() {
+    return (
+        <Card>
+            <CardHeader>
+                Melody Mystery ~
+            </CardHeader>
+            <CardBody></CardBody>
+        </Card>
+    )
+}
