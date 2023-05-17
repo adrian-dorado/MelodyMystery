84f2cd5a9b64b26e8253e9271b4784b17253f450
Adrian Dorado <rbbitphoto@gmail.com>
2023-05-16 22:33:38 -0700

    spotify pkce auth

--- melody-mystery/src/App.js
+++ melody-mystery/src/App.js
@@ -3,7 +3,7 @@ import { Flex, Container } from '@chakra-ui/react'
 import GameCard from './Components/GameCard';
 import LosingScreen from './Components/LosingScreen'
 import SpotifyLogin from './Spotify/SpotifyLogin';
-import { loginUrl } from './Spotify/SpotifyCreds';
+import { loginUrl, scopes } from './Spotify/SpotifyCreds';
 // import { spotifyGetUser } from './Spotify/SpotifyGetUser';
 
 export const GuessesContext = createContext();
@@ -16,65 +16,6 @@ function App() {
 
   const [guesses, setGuesses] = useState([])
 
-  console.log("This is the list of incorrect guesses", guesses)
-
-  // GetSpotifyUser()
-
-  const requestSpotifyAuthorization = () => {
-    window.location.href = loginUrl;
-  }
-
-  const spotifyGetToken = () => {
-
-    const hashParams = {};
-    const regex = /([^&;=]+)=?([^&;]*)/g;
-    const query = window.location.hash.substring(1);
-
-    let match;
-
-    while (match = regex.exec(query)) {
-      hashParams[match[1]] = decodeURIComponent(match[2]);
-    }
-
-    return hashParams.access_token;
-
-  }
-
-  function spotifyGetUserProfile(accessToken) {
-    const profileUrl = 'https://api.spotify.com/v1/me';
-
-    fetch(profileUrl, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Error: ' + response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('User Profile:', data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-
-  const accessToken = spotifyGetToken();
-
-  if (!accessToken) {
-    if (window.location.hash.indexOf('error') !== -1) {
-      // Display error message to the user
-      const errorMessage = decodeURIComponent(window.location.hash.split('&')[0].split('=')[1]);
-      console.error('Authorization Error:', errorMessage);
-    } else {
-      requestSpotifyAuthorization();
-    }
-  } else {
-    spotifyGetUserProfile(accessToken);
-  }
 
 
   return (
--- melody-mystery/src/Spotify/SpotifyCreds.js
+++ melody-mystery/src/Spotify/SpotifyCreds.js
@@ -1,8 +1,10 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
 
-const clientId = 'af23c74ca8c64f3d91c2447fe60d1457'
+export const clientId = 'af23c74ca8c64f3d91c2447fe60d1457'
 
-const redirectUri = 'http://localhost:3000'
+export const redirectUri = 'http://localhost:3000'
+
+export const redirectUriCallback = 'http://localhost:5173/callback'
 
 export const scopes = [
     'user-read-currently-playing',
@@ -10,6 +12,8 @@ export const scopes = [
     'user-read-playback-state',
     'user-top-read',
     'user-modify-playback-state',
+    'user-read-private',
+    'user-read-email',
 ]
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=token&scope=${scopes.join('%20')}&redirect_uri=${redirectUri}&show_dialog=true`
