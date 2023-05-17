058228088f1c011dd5f9084456ac96a18f91e829
Adrian Dorado <rbbitphoto@gmail.com>
2023-05-17 05:52:40 -0700

    saving for the night, need to move all auth into SpotifyRequestAuth and make *that* the provider. I hate it here

--- melody-mystery/src/Spotify/SpotifyRequestAuth.js
+++ melody-mystery/src/Spotify/SpotifyRequestAuth.js
@@ -13,74 +13,71 @@ export async function requestSpotifyAuthorization() {
         const accessToken = await spotifyGetAccessToken(clientId, code);
         profile = await fetchProfile(accessToken);
         console.log(profile);
-        // setSpotifyUser(profile);
+
     }
-    // setSpotifyUser(profile);
-    // setSpotifyUser(profile)
 
-}
+    async function redirectToSpotifyAuthFlow(clientId) {
+        const verifier = generateCodeVerifier(128);
+        const challenge = await generateCodeChallenge(verifier)
 
-async function redirectToSpotifyAuthFlow(clientId) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier)
+        localStorage.setItem('verifier', verifier);
 
-    localStorage.setItem('verifier', verifier);
+        const params = new URLSearchParams();
+        params.append('client_id', clientId);
+        params.append('response_type', 'code');
+        params.append('redirect_uri', redirectUri);
+        params.append('scope', scopes.join(' '));
+        params.append('code_challenge_method', 'S256');
+        params.append('code_challenge', challenge);
 
-    const params = new URLSearchParams();
-    params.append('client_id', clientId);
-    params.append('response_type', 'code');
-    params.append('redirect_uri', redirectUri);
-    params.append('scope', scopes.join(' '));
-    params.append('code_challenge_method', 'S256');
-    params.append('code_challenge', challenge);
+        window.location.href = `${authEndpoint}?${params.toString()}`;
 
-    window.location.href = `${authEndpoint}?${params.toString()}`;
-
-}
-
-function generateCodeVerifier(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-    return text;
-}
 
-async function generateCodeChallenge(codeVerifier) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
+    function generateCodeVerifier(length) {
+        let text = '';
+        let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-async function spotifyGetAccessToken(clientId, code) {
-    const verifier = localStorage.getItem('verifier');
+        for (let i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
 
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", redirectUri);
-    params.append("code_verifier", verifier);
+    async function generateCodeChallenge(codeVerifier) {
+        const data = new TextEncoder().encode(codeVerifier);
+        const digest = await window.crypto.subtle.digest('SHA-256', data);
+        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=+$/, '');
+    }
 
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
-    });
+    async function spotifyGetAccessToken(clientId, code) {
+        const verifier = localStorage.getItem('verifier');
 
-    const { access_token } = await result.json();
-    return access_token;
-}
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("grant_type", "authorization_code");
+        params.append("code", code);
+        params.append("redirect_uri", redirectUri);
+        params.append("code_verifier", verifier);
 
-async function fetchProfile(token) {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+        const result = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: params
+        });
 
-    return await result.json();
+        const { access_token } = await result.json();
+        return access_token;
+    }
+
+    async function fetchProfile(token) {
+        const result = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
+
+        return await result.json();
+    }
 }
